7091a225b3b14bdecdebd38e6432be5faad5626e
Sander Brito <37093580+SanderBrito@users.noreply.github.com>
2023-12-11 00:04:13 -0300

    Inserção do comentário Login
    Inserção do comentário Login no swagger

--- routes/login.js
+++ routes/login.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken')
 
 
 router.post("/login", (req, res) => {
+    // #swagger.summary = 'Gera um JWT se as condições de login forem atendidas'
     let {usuario, senha} = req.body
     if (usuario && senha && usuario !== senha && senha.length >= 5) {
         let token = jwt.sign({ usuario: usuario }, 'calvin', { expiresIn: '15 min' });
