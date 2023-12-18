f12f980076af17be01a9942f5b768aa2ffe1abb9
Sander Brito <37093580+SanderBrito@users.noreply.github.com>
2023-12-18 12:04:52 -0300

    Ajuste de conexão da coleção "user"
    Ajuste feito para conexão da coleção "user" com o banco de dados.

--- model/User.js
+++ model/User.js
@@ -1,50 +1,97 @@
-let userCounter = 0;
-let usersDatabase = [];
+const User = require('../controller/user')
 
-const UserManager = {
-    createUser: function (name, mail, pass) {
-        let newUser = { id: ++userCounter, name, mail, pass };
-        usersDatabase.push(newUser);
-        return newUser;
-    },
-    updateUser: function (id, name, mail, pass) {
-        let index = this.getIndexById(id);
-        if (index >= 0) {
-            let updatedUser = { id: parseInt(id), name, mail, pass };
-            usersDatabase[index] = updatedUser;
-            return updatedUser;
-        }
-        return null;
-    },
-    getAllUsers: function () {
-        return usersDatabase;
-    },
-    getUsersByName: function (name) {
-        return usersDatabase.filter(item => item.name.toUpperCase().startsWith(name.toUpperCase()));
-    },
-    getUsersByMail: function (mail) {
-        return usersDatabase.filter(item => item.mail === mail);
-    },
-    getUserById: function (id) {
-        let index = this.getIndexById(id);
-        return index >= 0 ? usersDatabase[index] : null;
-    },
-    getIndexById: function (id) {
-        for (let i = 0; i < usersDatabase.length; i++) {
-            if (usersDatabase[i].id == id) {
-                return i;
-            }
-        }
-        return -1;
-    },
-    deleteUser: function (id) {
-        let index = this.getIndexById(id);
-        if (index >= 0) {
-            usersDatabase.splice(index, 1);
-            return true;
-        }
-        return false;
+const createUser = async (req, res) =>{   
+    console.log(req.body.name_user)
+    const name_user = req.body.name_user;
+    const mail_user = req.body.mail_user;
+    const pass_user = req.body.pass_user;
+    const user ={
+        name_user,
+        mail_user,
+        pass_user,
     }
-};
+    try{
+        await User.create(user)
 
-module.exports = UserManager;
+        res.status(201).json({message: "usuário cadastrado com sucesso!"})
+
+    }catch (error) {
+        res.status(500).json({error: error})
+    }
+}
+
+const findUser = async (req, res) =>{
+    try {
+        const user = await User.find()
+        return user
+
+    } catch (error) {
+        res.status(500).json({ error: error})
+    }
+}
+
+const findOnlyUser = async (req, res) =>{
+    const name_user = req.body
+    console.log(name_user)
+
+    try {
+        const user = await User.findOne({ name_user: name_user })
+
+        if(!user) {
+            res.status(422).json({ message: 'usuário não encontrado!'})
+        }
+        return user
+        
+    } catch (error) {
+        res.status(500).json({ error: error})
+    }
+}
+
+
+const updateUser = async (req, res) =>{
+    const {name_user, mail_user, pass_user} = req.body
+
+
+    const user = {
+        name_user,
+        mail_user,
+        pass_user,        
+    }
+
+    try {
+        const updateUser = await User.updateOne({ _id: req.params.id }, user)
+
+        res.status(200).json({message: 'Cadastro Atualizado'})
+        
+    } catch (error) {
+        res.status(500).json({ error: error})
+    }
+}
+
+
+
+const deleteUser = async (req, res) =>{
+
+    const user = await User.findOne({ _id: req.params.id })
+    if(!user){
+        res.status(422).json({ message: 'usuário não encontrado!' })
+        return
+    }
+
+    try{
+        await User.deleteOne({_id: req.params.id})
+
+        res.status(200).json({ message: 'usuário deletado!' })
+    }catch(error){
+        res.status(500).json({ error: error })
+    }
+
+}
+
+module.exports ={
+    createUser,
+    findUser,
+    findOnlyUser,
+    updateUser,
+    deleteUser
+}
--- routes/user.js
+++ routes/user.js
@@ -4,41 +4,14 @@ const router = express.Router();
 const Token = require('../utilities/Token');
 const User = require('../model/User');
 
-router.get('/', (req, res) => {
-    // #swagger.summary = 'Método GET para listar usuários por nome ou e-mail'
-    let userList = User.getAllUsers();
+router.get('/', async (req, res) =>{
+    const user = await User.findUser()
+    res.json(user)
+})
+router.post('/', Token.validateAccess, User.createUser)
+router.delete('/:id', User.deleteUser)
+router.put('/:id', User.updateUser)
 
-    if (req.query.name) {
-        userList = User.getUsersByName(req.query.name);
-    } else if (req.query.mail) {
-        userList = User.getUsersByMail(req.query.mail);
-    }
-
-    res.json({ count: userList.length, users: userList });
-});
-
-router.get('/:id', (req, res) => {
-    // #swagger.summary = 'Método para listar por ID'
-    let user = User.getUserById(req.params.id);
-
-    if (user) {
-        res.json({ user: user });
-    } else {
-        res.status(404).json({ message: "O ID informado não é válido" });
-    }
-});
-
-router.post('/', Token.validateAccess, (req, res) => {
-     // #swagger.summary = 'Método PUT para cadastrar um usuário'
-    let { name, mail, pass } = req.body;
-
-    if (name && mail && pass) {
-        let newUser = User.createUser(name, mail, pass);
-        res.json({ user: newUser });
-    } else {
-        res.status(400).json({ message: "Erro ao cadastrar usuário" });
-    }
-});
 
 router.put('/:id', Token.validateAccess, (req, res) => {
     // #swagger.summary = 'Método PUT para atualizar o cadastro de um usuário através do ID'
@@ -54,18 +27,7 @@ router.put('/:id', Token.validateAccess, (req, res) => {
             res.status(400).json({ message: "O ID informado não foi encontrado" });
         }
     } else {
-        res.status(400).json({ message: "Falha ao alterar o registro na usuário" });
-    }
-});
-
-router.delete('/:id', Token.validateAccess, (req, res) => {
-    // #swagger.summary = 'Método DELETE para deletar um usuário através do seu ID'
-    let id = req.params.id;
-
-    if (User.deleteUser(id)) {
-        res.json({ message: "Elemento excluído com sucesso" });
-    } else {
-        res.status(400).json({ message: "Falha ao excluir o registro" });
+        res.status(400).json({ message: "Falha ao alterar o registro no usuário" });
     }
 });
 
